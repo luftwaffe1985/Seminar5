84a92e451547e43b83d43d2d9563254a52d6acea
luftwaffe1985 <putik1985@yandex.ru>
2023-01-10 18:21:16 +0300

    New Commit

--- src/countLinesInWords.java
+++ src/countLinesInWords.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class countLinesInWords {
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class countLinesInWords {
+    public static void main(String[] args) {
+        
+    }
+    public int countWords(String[] words1, String[] words2) {
+        Map<String, Integer> someMap1 = new HashMap<>();
+        Map<String, Integer> someMap2 = new HashMap<>();
+
+        for (String word : words1) {
+            someMap1.putIfAbsent(word, 0);
+            someMap1.put(word, someMap1.get(word) + 1);
+        }
+
+        for (String word : words2) {
+            someMap2.putIfAbsent(word, 0);
+            someMap2.put(word, someMap2.get(word) + 1);
+        }
+        int count = 0;
+
+        for (String word : words1) {
+            if (someMap1.containsKey(word) && someMap2.containsKey(word)) {
+                if (someMap1.get(word) == 1 && someMap2.get(word) == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
